b093f8a2816edcbfaa30693635799e8deefb143c
SergeyRushev <rushevs198417@gmail.com>
2023-06-22 14:27:33 +0300

    add: relationship between models Student and StudentClass

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\StudentClass;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Student extends Model
 {
     use HasFactory;
+
+    public function studentClass()
+    {
+        return $this->belongsTo(StudentClass::class);
+    }
 }
--- app/Models/StudentClass.php
+++ app/Models/StudentClass.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Student;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class StudentClass extends Model
 {
     use HasFactory;
+
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
 }
