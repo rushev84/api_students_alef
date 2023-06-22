0542b30bbb37cc27480c2b56d54d2ef49702da5f
SergeyRushev <rushevs198417@gmail.com>
2023-06-22 14:20:23 +0300

    feat: receiving info about chosen student

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,9 +7,17 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public function index()
+    public function index(): \Illuminate\Http\JsonResponse
     {
         $students = Student::all();
         return response()->json($students);
     }
+
+    public function show(int $id): \Illuminate\Http\JsonResponse
+    {
+        $student = Student::findOrFail($id);
+        return response()->json($student);
+    }
+
 }
+
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/students', [StudentController::class, 'index'])->name('students');
+Route::get('/students/{id}', [StudentController::class, 'show'])->name('student.show');
