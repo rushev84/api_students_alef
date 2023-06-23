7e979e42440c96238eb715bd2aac73b013853c8f
SergeyRushev <rushevs198417@gmail.com>
2023-06-23 11:08:16 +0300

    feat: json-response when validation is failed

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,10 +6,22 @@ use App\Models\Lecture;
 use App\Models\Student;
 use App\Models\Curriculum;
 use Illuminate\Http\Request;
+// use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\Validator;
+use App\Http\Requests\CreateStudentRequest;
 use Illuminate\Validation\ValidationException;
 
 class StudentController extends Controller
 {
+    protected function failedValidation(Validator $validator)
+    {
+        return new ValidationException($validator, response()->json([
+            'success' => false,
+            'message' => 'Validation failed',
+            'errors' => $validator->errors(),
+        ]));
+    }
+
     public function index(): \Illuminate\Http\JsonResponse
     {
         $students = Student::all();
@@ -36,26 +48,39 @@ class StudentController extends Controller
         return response()->json($student);
     }
 
-    public function store(Request $request): string
+    // public function store(Request $request): string
+    // {
+    //     try {
+    //         $validated = $request->validate([
+    //             'name' => ['required', 'max:255', 'regex:/^[^0-9]*$/'],
+    //             'email' => 'required|email|unique:students,email',
+    //             'student_class_id' => 'required|integer|exists:student_classes,id',
+    //         ]);
+
+    //         Student::create([
+    //             'name' => $validated['name'],
+    //             'email' => $validated['email'],
+    //             'student_class_id' => $validated['student_class_id'],
+    //         ]);
+
+    //         return 'Студент успешно создан!';
+
+    //     } catch (ValidationException $e) {
+    //         return $e->getMessage();
+    //     }
+    // }
+
+    public function store(CreateStudentRequest $request)
     {
-        try {
-            $validated = $request->validate([
-                'name' => ['required', 'max:255', 'regex:/^[^0-9]*$/'],
-                'email' => 'required|email|unique:students,email',
-                'student_class_id' => 'required|integer|exists:student_classes,id',
-            ]);
 
-            Student::create([
-                'name' => $validated['name'],
-                'email' => $validated['email'],
-                'student_class_id' => $validated['student_class_id'],
-            ]);
+        Student::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'student_class_id' => $request->input('student_class_id'),
+        ]);
 
-            return 'Студент успешно создан!';
+        return 'Студент успешно создан!';
 
-        } catch (ValidationException $e) {
-            return $e->getMessage();
-        }
     }
 }
 
---
+++ app/Http/Requests/CreateStudentRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateStudentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'max:255', 'regex:/^[^0-9]*$/'],
+            'email' => 'required|email|unique:students,email',
+            'student_class_id' => 'required|integer|exists:student_classes,id',
+        ];
+    }
+}
