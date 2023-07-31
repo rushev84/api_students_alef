8c5b27eee73a40cfdf23f603ff3566963bd9afa9
SergeyRushev <rushevs198417@gmail.com>
2023-07-31 10:56:10 +0300

    docs: classes

--- app/Http/Controllers/Swagger/StudentClassController.php
+++ app/Http/Controllers/Swagger/StudentClassController.php
@@ -83,7 +83,6 @@ use App\Http\Controllers\Controller;
  *             @OA\Property(property="students", type="array", @OA\Items(
  *                 @OA\Property(property="id", type="string"),
  *                 @OA\Property(property="name", type="string"),
- *                 @OA\Property(property="email", type="string"),
  *                 ),
  *                 example={
  *                     {
@@ -154,6 +153,61 @@ use App\Http\Controllers\Controller;
  *         description="OK",
  *         @OA\JsonContent(
  *             @OA\Property(property="message", type="string", example="Класс успешно удалён!"),
+ *         ),
+ *     ),
+ * ),
+ *
+ * @OA\Get(
+ *     path="/api/classes/{id}/curriculum",
+ *     summary="Получение учебного плана (списка лекций) для конкретного класса",
+ *     tags={"Классы"},
+ *     @OA\Parameter(
+ *         description="id класса",
+ *         in="path",
+ *         name="id",
+ *         required=true,
+ *         example=1,
+ *     ),
+ *
+ *
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="id", type="integer", example="3"),
+ *             @OA\Property(property="name", type="string", maxLength=255, example="02-СВ"),
+ *             @OA\Property(property="curriculums", type="array", @OA\Items(
+ *                 @OA\Property(property="order", type="string"),
+ *                 @OA\Property(property="lecture", type="array", @OA\Items(
+ *                         @OA\Property(property="id", type="integer"),
+ *                         @OA\Property(property="topic", type="string"),
+ *                         @OA\Property(property="description", type="string"),
+ *                         ),
+ *                     ),
+ *                 ),
+ *                 example={
+ *                     "id": 1,
+ *                     "name": "02-СВ",
+ *                     "curriculums": {
+ *                         {
+ *                             "order": 1,
+ *                             "lecture": {
+ *                                 "id": 6,
+ *                                 "topic": "Введение в ООП",
+ *                                 "description": "В этой лекции вы познакомитесь с объектно ориентированным программированием на PHP. Вы узнаете о классах, интерфейсах, инкапсуляции и магических методах. В итоге поймете, для чего нужны классы как абстракция данных. Знание основ объектно ориентированного программирования пригодится, чтобы понимать плюсы и минусы кода с классами и объектами. Также это поможет использовать объекты для реализации программной логики. Этот курс подойдет тем, кто уже хорошо знаком с языком PHP. Чтобы учиться было проще, стоит заранее изучить курсы: «Основы PHP», «PHP: Массивы», «PHP: Ассоциативные массивы» и «PHP: Функции».",
+ *                             },
+ *                         },
+ *                         {
+ *                             "order": 2,
+ *                             "lecture": {
+ *                                 "id": 5,
+ *                                 "topic": "Объектно-ориентированный дизайн",
+ *                                 "description": "В этой лекции вы познакомитесь с объектно ориентированным дизайном. Вы узнаете о паттернах, текучем интерфейсе и полезных популярных библиотеках. В итоге поймете, как правильно организовывать код, написанный на классах. Знания объектно ориентированного дизайна пригодятся, чтобы создавать и использовать цепочки функций, в том числе в неизменяемом стиле. Также они помогут правильно организовывать состояние объектов и проектировать безопасные сеттеры. Этот курс подойдет тем, кто уже хорошо знаком с языком PHP. Чтобы учиться было проще, стоит заранее изучить курсы: «Основы PHP», «PHP: Массивы», «PHP: Ассоциативные массивы» и «PHP: Функции».",
+ *                             },
+ *                         },
+ *                     },
+ *                 }
+ *             ),
  *         ),
  *     ),
  * ),
