cca9f9e4dd43e97e213d0e392febfcf9e81ea00e
SergeyRushev <rushevs198417@gmail.com>
2023-07-31 08:25:52 +0300

    docs: get student

--- app/Http/Controllers/Swagger/StudentController.php
+++ app/Http/Controllers/Swagger/StudentController.php
@@ -23,6 +23,41 @@ use App\Http\Controllers\Controller;
  *     ),
  * ),
  *
+ * @OA\Get(
+ *     path="/api/students/{id}",
+ *     summary="Получение информации о конкретном студенте (имя, email, класс, прослушанные лекции)",
+ *     tags={"Студенты"},
+ *     @OA\Parameter(
+ *         description="id студента",
+ *         in="path",
+ *         name="id",
+ *         required=true,
+ *         example=1,
+ *     ),
+ *
+ *
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="id", type="integer", example="1"),
+ *             @OA\Property(property="name", type="string", maxLength=255, pattern="^[^0-9]*$", example="Иван Иванов"),
+ *             @OA\Property(property="email", type="string", format="email", example="ivanov@mail.ru"),
+ *             @OA\Property(property="student_class", type="object",
+ *                 @OA\Property(property="id", type="integer", example=3),
+ *                 @OA\Property(property="name", type="string", example="02-СВ"),
+ *             ),
+ *             @OA\Property(property="lectures", type="array", @OA\Items(
+ *                 @OA\Property(property="id", type="integer", example=1),
+ *                 @OA\Property(property="topic", type="string", example="Введение в ООП"),
+ *                 @OA\Property(property="description", type="string", example="В этой лекции вы познакомитесь с объектно ориентированным программированием на PHP. Вы узнаете о классах, интерфейсах, инкапсуляции и магических методах. В итоге поймете, для чего нужны классы как абстракция данных. Знание основ объектно ориентированного программирования пригодится, чтобы понимать плюсы и минусы кода с классами и объектами. Также это поможет использовать объекты для реализации программной логики. Этот курс подойдет тем, кто уже хорошо знаком с языком PHP. Чтобы учиться было проще, стоит заранее изучить курсы: «Основы PHP», «PHP: Массивы», «PHP: Ассоциативные массивы» и «PHP: Функции»."),
+ *                 ),
+ *             ),
+ *         ),
+ *     ),
+ *
+ * ),
+ *
  * @OA\Post(
  *     path="/api/students",
  *     summary="Создание студента",
